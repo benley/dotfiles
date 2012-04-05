6fdedef8f30a05ddd937ea24fd2adcde2720a99f
Benjamin Staffin <benley@zoiks.net>
2012-04-04 17:39:03 -0700

    Be significantly more clever in the install script.
    * Only prompt to overwrite a file if it's actually necessary.
    * New functions that are probably going to go into a common library at some
      point:
      - SameFile: determines if two files point at the same inode (symlink,
        hardlink, same file)
      - SymLink: symlinks one file to another, either forcefully or prompting
        first.
      - SymlinkIfDiffer: Using SameFile and SymLink, updates a symlink if it
        didn't exist, or it pointed at something else.
    * Handle MacOS and Linux, and punt on other OSes.
    * Fix a typo: -h isn't present in both gnu and bsd ln, but -n is.

--- install.sh
+++ install.sh
@@ -2,21 +2,66 @@
 
 set -o errexit
 
-LN="ln -sih"
+force=0
 if [[ "$1" == "-f" ]] || [[ "$1" == "--force" ]]; then
-  LN="ln -sfn"
+  readonly force=1
 fi
 
 set -o nounset
 
-for file in .bash_logout .bash_profile .bashrc .dircolors .pythonrc.py .screenrc .tmuxrc; do
-  $LN "$PWD/$file" "$HOME/$file"
-done
+OS=$(uname)
+case $OS in
+  "Darwin"|"Linux")
+  : # Do nothing.
+  ;;
+  *)
+  echo "ERROR: unknown OS: \"${OS}\"; can't continue." >> /dev/stderr
+  exit 1
+  ;;
+esac
+
+function SameFile() {
+  # Return 0 if $1 and $2 are hard or soft links to the same file.
+  # (That is, if they point at the same inode, or are literally the same file.)
+  case $OS in
+    "Darwin")
+      inode1=$(stat -L -f%i $1 2>/dev/null)
+      inode2=$(stat -L -f%i $2 2>/dev/null)
+      ;;
+    "Linux")
+      inode1=$(stat -L -c%i $1 2>/dev/null)
+      inodw2=$(stat -L -c%i $2 2>/dev/null)
+      ;;
+    *)
+      echo "ERROR: unknown OS: \"$OS\"" >> /dev/stderr
+  esac
+  [[ $inode1 -eq $inode2 ]] && return 0 || return 1
+}
+
+function Symlink() {
+  # Symlink $1 to $2.
+  # If force=1, don't confirm before overwriting existing files.
+  if ((force)); then
+    ln -sfn "$1" "$2"
+  else
+    ln -sin "$1" "$2"
+  fi
+}
+
+function SymlinkIfDiffer() {
+  # If $1 and $2 are not the same file or symlinks to the same file, symlink $1
+  # to $2.
+  if ! SameFile "$1" "$2"; then
+    Symlink "$1" "$2"
+  fi
+}
 
 mkdir -p "$HOME/bin"
-for file in bin/*; do
-  $LN "$PWD/$file" "$HOME/$file"
+for file in bin/* .inputrc .bash_logout .bash_profile .bashrc .dircolors .pythonrc.py .screenrc .tmuxrc; do
+  src="$PWD/$file"
+  dst="$HOME/$file"
+  SymlinkIfDiffer "$PWD/$file" "$HOME/$file"
 done
 
-$LN "$PWD/vim/vimrc" "$HOME/.vimrc"
-$LN "$PWD/vim/dotvim" "$HOME/.vim"
+SymlinkIfDiffer "$PWD/vim/vimrc" "$HOME/.vimrc"
+SymlinkIfDiffer "$PWD/vim/dotvim" "$HOME/.vim"
