e03dfdb4e7f2ba05d320e22015fe31f296a8d670
Benjamin Staffin <ben@cloudscaling.com>
2014-06-30 01:55:12 -0700

    Xmonad update from ein

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,39 +2,40 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.NoBorders
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Layout.Tabbed
 import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageHelpers
+import XMonad.Util.Run(spawnPipe)
 import System.Exit
 import System.IO
--- TBD:
---import XMonad.Layout.Tabbed
 
 --import Data.Monoid
 --import qualified XMonad.StackSet as W
 --import qualified Data.Map as M
 
 myManageHook = composeAll
-    [ manageHook gnomeConfig <+> manageDocks,
+    [ manageHook gnomeConfig,
+      fullscreenManageHook,
       isFullscreen --> doFullFloat,
+      isDialog     --> doCenterFloat,
       resource  =? "FEZ.bin.x86"    --> doFloat,
       resource  =? "FEZ.bin.x86_64" --> doFloat,
       resource  =? "tinyandbig"     --> doIgnore,
       className =? "Gimp"           --> doFloat,
       className =? "hl2_linux"      --> doFullFloat,
       className =? "steam"          --> doIgnore,  -- big picture mode?
+      className =? "Steam"          --> doFloat,
       manageDocks
     ]
 
-myLayoutHook = smartBorders (
-                   avoidStruts (
-                       Tall 1 (3/100) (1/2) ||| Mirror (Tall 1 (3/100) (1/2))
-                       ) ||| noBorders (fullscreenFull Full)
-                   )
+myLayoutHook = smartBorders $ avoidStruts (
+       Tall 1 (3/100) (1/2) ||| Mirror (Tall 1 (3/100) (1/2)) ||| simpleTabbed
+       ) ||| noBorders (fullscreenFull Full)
+
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/benley/.xmobarrc"
     xmonad $ ewmh gnomeConfig {
@@ -42,11 +43,13 @@ main = do
                 manageHook = myManageHook,
                 handleEventHook = XMonad.Hooks.EwmhDesktops.fullscreenEventHook,
                 layoutHook = myLayoutHook,
-                logHook = dynamicLogWithPP xmobarPP
+                logHook = do
+                  dynamicLogWithPP xmobarPP
                     { ppOutput = hPutStrLn xmproc,
                       ppTitle = xmobarColor "green" ""
                       -- add . shorten 70 above to limit title length in xmobar
                     }
+                  logHook desktopConfig
         } `additionalKeys`
         [ ((mod1Mask .|. mod4Mask .|. shiftMask, xK_l), spawn "gnome-screensaver-command --lock"),
           ((mod1Mask .|. shiftMask, xK_q), io (exitWith ExitSuccess)),
