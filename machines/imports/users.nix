c7213ba6b7971e876d4f8e6f527ad9cddb86d6a0
Benjamin Staffin <benley@gmail.com>
2017-02-23 20:51:20 -0500

    password hash note

--- machines/imports/users.nix
+++ machines/imports/users.nix
@@ -6,6 +6,8 @@
     uid = 1000;
     description = "Benjamin Staffin";
     extraGroups = [ "wheel" "vboxusers" ];
+
+    # Just a bootstrapping password, not one I actually use for anything:
     initialHashedPassword =
       ''$6$cfJfcXyI7KvBD$1MuMJk3VBn.VeF6LNbeuLeo0BZGzdZbz4AP/UDChAnlI73onkiIxDUthkAFUtR/RN.20.lnyS3550EEPbVIVQ.'';
   };
