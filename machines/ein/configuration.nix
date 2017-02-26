8182c4c13a8d78f23f4fd5db4829110a09d9ca1b
Benjamin Staffin <benley@gmail.com>
2017-02-26 02:40:16 -0500

    sweet, got the Steam controller working

--- machines/ein/configuration.nix
+++ machines/ein/configuration.nix
@@ -34,6 +34,41 @@ let
     '';
   };
 
+  steamcontroller-udev-rules = pkgs.writeTextFile {
+    name = "steamcontroller-udev-rules";
+    text = ''
+      # This rule is needed for basic functionality of the controller in
+      # Steam and keyboard/mouse emulation
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
+
+      # This rule is necessary for gamepad emulation
+      KERNEL=="uinput", MODE="0660", GROUP="wheel" OPTIONS+="static_node=uinput"
+      # systemd option not yet tested
+      #KERNEL=="uinput", SUBSYSTEM="misc", TAG+="uaccess", TAG+="udev-acl"
+
+      # Valve HID devices over USB hidraw
+      KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666"
+
+      # Valve HID devices over bluetooth hidraw
+      KERNEL=="hidraw*", ATTRS{idVendor}=="*28DE:*", MODE="0666"
+
+      # DualShock 4 over USB hidraw
+      KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0666"
+
+      # DualShock 4 wireless adapter over USB hidraw
+      KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ba0", MODE="0666"
+
+      # DualShock 4 Slim over USB hidraw
+      KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="09cc", MODE="0666"
+
+      # DualShock 4 over bluetooth hidraw
+      KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0666"
+
+      # DualShock 4 Slim over bluetooth hidraw
+      KERNEL=="hidraw*", KERNELS=="*054C:09CC*", MODE="0666"
+    '';
+    destination = "/etc/udev/rules.d/99-steamcontroller.rules";
+  };
 in
 
 {
@@ -129,6 +164,8 @@ in
   services.openssh.enable = true;
 
   services.printing.enable = true;
+
+  services.udev.packages = [ steamcontroller-udev-rules ];
 
   services.xserver = {
     enable = true;
