2982a6017af79effc23274e6af535c2635bded3b
Benjamin Staffin <benley@gmail.com>
2017-03-08 11:47:49 -0500

    Get nix-home from github

--- pkgs/nix-home/default.nix
+++ pkgs/nix-home/default.nix
@@ -4,7 +4,13 @@ stdenv.mkDerivation rec {
   version = "0.3.2";
   name = "nix-home-${version}";
 
-  src = /home/benley/p/nix-home;
+  src = fetchFromGitHub {
+    rev = "f84fc9059bd3c7aec403c032748715eb3a2d2b95";
+    repo = "nix-home";
+    owner = "benley";
+    sha256 = "0ad8rnhwy6na9yw5a6rqprcrmjv2wgjg4djn37dqwf3fq2vg1z8w";
+  };
+
   # src = fetchFromGitHub {
   #   rev = version;
   #   repo = "nix-home";
@@ -31,7 +37,7 @@ stdenv.mkDerivation rec {
   meta = {
     homepage = https://github.com/sheenobu/nix-home;
     description = "Per-user configuration management via Nix";
-    licenses = [ stdenv.lib.license.mit ];
+    license = stdenv.lib.licenses.mit;
     platforms = stdenv.lib.platforms.unix;
     inherit version;
   };
