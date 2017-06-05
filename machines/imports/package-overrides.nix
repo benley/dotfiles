760d76e10cebeec368c8ac12f6dfc81c2e3ebb05
Benjamin Staffin <benley@gmail.com>
2017-06-05 00:03:42 -0400

    meh

--- machines/imports/package-overrides.nix
+++ machines/imports/package-overrides.nix
@@ -1,18 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  nixpkgs.config.packageOverrides = origPkgs: with origPkgs; {
+  nixpkgs.config.packageOverrides = pkgs: {
 
     # This would enable kerberos in the default openssh package so it gets
     # included with things like git. Unfortunately, that causes nix to rebuild
     # a ton of stuff, which takes *forever*.
     #
-    # openssh = openssh.override {
+    # openssh = pkgs.openssh.override {
     #   withKerberos = true;
     #   withGssapiPatches = true;
     # };
 
     # remove this once docker >= 17.05 becomes the normal docker version
-    docker = origPkgs.docker_17_05;
+    docker = pkgs.docker_17_05;
   };
 }
