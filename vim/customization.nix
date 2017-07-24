6e93203e55230e24d707c90f7019ea3ce5759196
Benjamin Staffin <benley@gmail.com>
2017-07-23 21:46:08 -0400

    sort plugin list

--- vim/customization.nix
+++ vim/customization.nix
@@ -9,25 +9,20 @@ in {
     knownPlugins = pkgs.vimPlugins // plugins;
 
     pluginDictionaries = [
-      # YCM triggers a bug in neovim:
-      # https://github.com/neovim/neovim/issues/6166
-      # { name = "youcompleteme"; }
-      { name = "vim-trailing-whitespace"; }
-      { name = "Solarized"; }
-      { name = "Supertab"; }
-      { name = "Syntastic"; }
-      { name = "Tagbar"; }
-      { name = "The_NERD_tree"; }
       { name = "fugitive"; }
       { name = "ghcmod"; }
       { name = "molokai"; }
       { name = "neco-ghc"; }
       { name = "rainbow_parentheses"; }
       { name = "rust-vim"; }
+      { name = "Solarized"; }
+      { name = "Supertab"; }
+      { name = "Syntastic"; }
       { name = "tabular"; }
+      { name = "Tagbar"; }
       { name = "taglist"; }
+      { name = "The_NERD_tree"; }
       # { name = "vim-addon-nix"; } # The MarcWeber version
-      { name = "vim-nix"; }  # The LnL7 one
       { name = "vim-airline"; }
       { name = "vim-airline-themes"; }
       { name = "vim-buffergator"; }
@@ -35,11 +30,16 @@ in {
       { name = "vim-eunuch"; }
       { name = "vim-gitgutter"; }
       { name = "vim-go"; }
+      # { name = "vim-hdevtools"; }
       { name = "vim-jsonnet"; }
+      { name = "vim-nix"; }  # The LnL7 one
+      { name = "vim-trailing-whitespace"; }
       { name = "vim2hs"; }
       { name = "vimproc"; }
       { name = "vimshell-vim"; }
-      # { name = "vim-hdevtools"; }
+      # YCM triggers a bug in neovim:
+      # https://github.com/neovim/neovim/issues/6166
+      # { name = "youcompleteme"; }
       { name = "zenburn"; }
     ];
   };
