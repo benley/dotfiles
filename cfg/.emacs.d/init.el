78543a50ff0095703f735a3735b609ed0dab6bfd
Benjamin Staffin <benley@gmail.com>
2017-12-13 15:26:16 -0500

    more emacs faffery

--- cfg/.emacs.d/init.el
+++ cfg/.emacs.d/init.el
@@ -1,4 +1,7 @@
-(require 'cl)
+;;; init.el --- emacs init
+;;; Commentary:
+;;; Code:
+
 (require 'package)
 
 (setq package-archives
@@ -7,51 +10,127 @@
         ("org"   . "http://orgmode.org/elpa/")))
 (package-initialize)
 
-(when (not package-archive-contents)
-  (package-refresh-contents))
+(unless (require 'use-package nil t)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(defvar my-packages
-  '(base16-theme
-    clojure-mode
-    clojure-mode-extra-font-locking
-    cider
-    company
-    company-emoji
-    emojify
-    ;;evil
-    haskell-mode
-    ido-ubiquitous
-    magit
-    nix-mode
-    ;;noctilux-theme
-    paredit
-    ;;projectile
-    puppet-mode
-    rainbow-delimiters
-    ;;smex
-    ;;tagedit
-    web-mode
-    ;;anti-zenburn-theme
-    yaml-mode
-    ;;zenburn-theme
-    ;;solarized-theme
-    flycheck
-    flycheck-color-mode-line
-    flycheck-pos-tip
-    powerline
-    ;;(if (eq system-type 'darwin) exec-path-from-shell)
-    ))
+(setq use-package-always-ensure t)
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(setq user-mail-address "benley@gmail.com")
 
-;(require 'evil)
-;(evil-mode 1)
+;;(use-package base16-theme
+;;  :config (load-theme 'base16-materia t))
 
-(require 'company-emoji)
-(add-to-list 'company-backends 'company-emoji)
-(setq company-emoji-insert-unicode nil)
+(use-package material-theme
+ :config (load-theme 'material t))
+
+;;(use-package clojure-mode)
+;;(use-package clojure-mode-extra-font-locking)
+
+(use-package company
+  :diminish company-mode
+  ;; Use company-mode in all buffers (more completion)
+  :config (add-hook 'after-init-hook #'global-company-mode))
+
+(use-package company-emoji
+  :config (add-to-list 'company-backends 'company-emoji)
+          (setq company-emoji-insert-unicode nil))
+
+(use-package diminish)
+
+(use-package emojify)
+
+;;(use-package evil
+;;  :config (evil-mode 1))
+
+(use-package flycheck
+  :diminish flycheck-mode
+  :config (global-flycheck-mode 1))
+
+(use-package flycheck-pos-tip
+  :config (flycheck-pos-tip-mode))
+
+(use-package flycheck-color-mode-line
+  :config (flycheck-color-mode-line-mode))
+
+(use-package go-mode)
+
+(use-package haskell-mode)
+
+(use-package ido-completing-read+
+  ;; Really nice completion for commands and whatnot
+  :init
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (setq ido-enable-flex-matching 1)
+
+  :config
+  ;; show ido autocomplete options pretty much everywhere, like when you hit M-x
+  (ido-ubiquitous-mode 1))
+
+(use-package jsonnet-mode)
+
+(use-package magit
+  :bind ("C-x g" . magit-status))
+
+(use-package markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :mode (("\\.md\\'" . gfm-mode))
+  :init (setq markdown-command "pandoc")
+  :config (add-hook 'gfm-mode-hook (lambda () (visual-line-mode 1))))
+
+(use-package nix-mode)
+
+(use-package nyan-mode
+ :config (nyan-mode 1))
+
+(use-package paredit)
+
+;; (use-package powerline
+;;   :config (powerline-default-theme))
+
+(use-package spaceline-all-the-icons
+  :after spaceline
+  :config (spaceline-all-the-icons-theme))
+
+(use-package spaceline
+  ;; :init
+  ;; (setq powerline-default-separator 'wave)
+  ;; (setq powerline-gui-use-vcs-glyph t)
+  ;; :config
+  ;; (require 'spaceline-config)
+  ;; (spaceline-compile)
+  ;; (spaceline-spacemacs-theme)
+  )
+
+(use-package rainbow-delimiters
+  :config (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
+(use-package smex
+  ;; Put frequently-used commands at the front of ido completion list
+  :config
+  (smex-initialize)
+  (global-set-key (kbd "M-x") 'smex)
+  (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+  (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command))
+
+(use-package web-mode
+  :init
+  (defun custom-web-mode-hook ()
+    "Hooks for Web mode."
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
+    (setq web-mode-code-indent-offset 2))
+
+  :config
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+
+  :hook
+  web-mode-hook 'custom-web-mode-hook)
+
+(use-package yaml-mode)
 
 (defun --set-emoji-font (frame)
   "Adjust the font settings of FRAME so Emacs can display emoji properly."
@@ -65,29 +144,14 @@
 
 (add-hook 'after-make-frame-functions '--set-emoji-font)
 
-(require 'powerline)
-(powerline-default-theme)
-
-(with-eval-after-load 'flycheck
-  (flycheck-pos-tip-mode))
-
-(require 'flycheck-color-mode-line)
-(flycheck-color-mode-line-mode)
-
-(global-flycheck-mode 1)
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'base16-materia t)
-
 (tool-bar-mode 0)                       ;; disable toolbar
 (global-linum-mode 1)                   ;; show line numbers
 (global-hl-line-mode 1)                 ;; highlight current line
 (column-number-mode 1)                  ;; show column position in modeline
 (show-paren-mode 1)                     ;; highlight matching parens
-(rainbow-delimiters-mode 1)             ;; ((((rainbows))))
 
-(if (eq system-type 'darwin)
-    (setq dired-use-ls-dired nil))      ;; osx ls doesn't have --dired
+(when (eq system-type 'darwin)
+  (setq dired-use-ls-dired nil))        ;; ls doesn't have --dired on darwin
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (add-hook 'text-mode-hook (lambda () (turn-on-auto-fill)))
@@ -95,52 +159,35 @@
 ;; Stop littering everywhere with save files, put them somewhere
 (setq backup-directory-alist '(("." . "~/.emacs-backups")))
 
-;; Really nice completion for commands and whatnot
-(ido-mode 1)
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-
-;; Use company-mode in all buffers (more completion)
-(add-hook 'after-init-hook 'global-company-mode)
-
 ;; Remember what I had open when I quit
-(desktop-save-mode 1)
+;;(desktop-save-mode 1)
 
 ;; do not want
 (setq-default indent-tabs-mode nil)
 
-;; shut the FUCK UP
+;; oh my god STOP BEEPING
 (setq visible-bell 1)
-
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
-
-(defun custom-web-mode-hook ()
-  "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
-(add-hook 'web-mode-hook  'custom-web-mode-hook)
-
-(global-set-key (kbd "C-x g") 'magit-status)
 
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
 (setq mouse-wheel-progressive-speed nil)
 
-(custom-set-faces
- '(default ((t (:family "PragmataPro" :weight medium)))))
+(global-unset-key (kbd "C-z"))  ;; ctrl-z should *not* background a gui app
+(global-set-key (kbd "C-z C-z") 'my-suspend-frame)
+(defun my-suspend-frame ()
+  "Like 'suspend-frame', but refuses to work on graphical windows."
+  (interactive)
+  (if (display-graphic-p)
+      (message "nope, not backgrounding a gui frame")
+    (suspend-frame)))
 
-(custom-set-variables
- '(column-number-mode t)
- '(custom-safe-themes
-   (quote
-    ("d9dab332207600e49400d798ed05f38372ec32132b3f7d2ba697e59088021555" "cdbd0a803de328a4986659d799659939d13ec01da1f482d838b68038c1bb35e8" "5cd0afd0ca01648e1fff95a7a7f8abec925bd654915153fb39ee8e72a8b56a1f" "68d36308fc6e7395f7e6355f92c1dd9029c7a672cbecf8048e2933a053cf27e6" "6a925fdf3a7bf2f3901d8fbc4ef64f9b4b4be2c6bed2b0d49d154db0bec91b33" "19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" default)))
- '(mac-option-modifier (quote (:ordinary meta :function alt :mouse alt)))
- '(mac-right-option-modifier nil)
- '(org-agenda-files (quote ("~/Dropbox/org/stuff.org")))
- '(show-trailing-whitespace t))
+(defun server-shutdown ()
+  "Save buffers, quit, and shutdown the Emacs server."
+  (interactive)
+  (save-some-buffers)
+  (kill-emacs))
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 (provide 'init)
 ;;; init.el ends here
