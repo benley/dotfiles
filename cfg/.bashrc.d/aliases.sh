6cca050c584ac7c15e23786582f2960183a47af8
Benjamin Staffin <benley@gmail.com>
2018-01-02 17:11:44 -0500

    add amqping

--- cfg/.bashrc.d/aliases.sh
+++ cfg/.bashrc.d/aliases.sh
@@ -10,3 +10,11 @@ ssh-keyrm() {
     return 1
   fi
 }
+
+amqping() {
+  [[ $# -eq 1 && $# -le 2 ]] || { echo "USAGE: amqping <hostname> [port]" >&2; return 1; }
+  cmd="echo -e 'AMQP\x00\x00\x09\x01' | nc -w 1 '$1' '${2:-5672}' | cat -t"
+  echo "$cmd" >&2
+  eval $cmd
+  echo
+}
