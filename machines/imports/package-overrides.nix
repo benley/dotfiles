9bb0a584ad25e4bff4861b534761289e51522f8a
Benjamin Staffin <benley@gmail.com>
2018-06-10 16:00:45 -0400

    newer plexmediaserver

--- machines/imports/package-overrides.nix
+++ machines/imports/package-overrides.nix
@@ -22,5 +22,16 @@
       packages = _: [haskellPackages.taffybar-plugins];
     });
 
+    plex = super.plex.overrideAttrs (x: rec {
+      name = "plex-${version}";
+      version = "1.13.2.5102";
+      vsnHash = "4898d7d96";
+
+      src = pkgs.fetchurl {
+        url = "https://downloads.plex.tv/plex-media-server/${version}-${vsnHash}/plexmediaserver-${version}-${vsnHash}.x86_64.rpm";
+        sha256 = "16mr9g6scv9ihqdq2lzaca8kbanr405mfsz0gip7cf65z1z4fjnj";
+      };
+    });
+
   };
 }
