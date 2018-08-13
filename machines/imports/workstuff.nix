8c718e8717d056c7a357551e4c44913a8d419832
Benjamin Staffin <benley@gmail.com>
2018-08-13 12:22:23 -0400

    keep up with dnsmasq module stuff

--- machines/imports/workstuff.nix
+++ machines/imports/workstuff.nix
@@ -1,11 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  networking.networkmanager.useDnsmasq = true;
+  networking.networkmanager.dns = "dnsmasq";
   networking.networkmanager.enableStrongSwan = true;
 
   environment.etc = [{
     target = "NetworkManager/dnsmasq.d/work-vpn.conf";
     text = builtins.readFile ../../private/work-vpn.dnsmasq.conf;
   }];
+
+  # services.unbound.enable = true;
+  # services.unbound.extraConfig = builtins.readFile ../../private/postmates.unbound.conf;
 }
