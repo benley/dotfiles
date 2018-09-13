e7a9c9be3f51c1ef453cc2e6185edb8c732a50ef
Benjamin Staffin <benley@gmail.com>
2018-09-12 21:14:44 -0400

    add stupid x1c thermal fixes

--- machines/mintaka/configuration.nix
+++ machines/mintaka/configuration.nix
@@ -44,10 +44,18 @@ with {
     "mem_sleep_default=deep"
 
     "zswap.enabled=1"
-    "zswap.compressor=lz4"
     "zswap.max_pool_percent=25"
-    "zswap.zpool=z3fold"
+
+    # These next two have to happen in postBootCommands because the
+    # modules aren't compiled into the kernel
+    # "zswap.zpool=z3fold"
+    # "zswap.compressor=lz4"
   ];
+
+  boot.postBootCommands = ''
+    echo z3fold > /sys/module/zswap/parameters/zpool
+    echo lz4 > /sys/module/zswap/parameters/compressor
+  '';
 
   boot.initrd.prepend = ["${x1c_patched_dsdt}/x1c-dsdt.img"];
 
@@ -149,5 +157,37 @@ with {
   environment.systemPackages = [
     pkgs.thunderbolt
   ];
+
+  # Temporary fix for cpu throttling issues visible in the kernel log
+  # (journalctl -k) by setting the same temperature limits used by
+  # Windows
+  # See https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues
+  systemd.services.cpu-throttling = {
+    enable = true;
+    description = "Set temp offset to 3°C, so the new trip point is 97°C";
+    documentation = [
+      "https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues"
+    ];
+    path = [ pkgs.msr-tools ];
+    script = "wrmsr -a 0x1a2 0x3000000";
+    serviceConfig = {
+      Type = "oneshot";
+    };
+    wantedBy = [ "timers.target" ];
+  };
+
+  systemd.timers.cpu-throttling = {
+    enable = true;
+    description = "Set cpu throttling threshold to 97°C";
+    documentation = [
+      "https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues"
+    ];
+    timerConfig = {
+      OnActiveSec = 60;
+      OnUnitActiveSec = 60;
+      Unit = "cpu-throttling.service";
+    };
+    wantedBy = [ "timers.target" ];
+  };
 
 }
