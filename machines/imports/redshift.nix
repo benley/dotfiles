90dd45773f9039c07d075a790a81dbbe562cbe7f
Benjamin Staffin <benley@gmail.com>
2019-08-14 13:13:33 -0400

    redshift: 6500k during daytime

--- machines/imports/redshift.nix
+++ machines/imports/redshift.nix
@@ -5,7 +5,7 @@
     enable = true;
     latitude = lib.mkDefault "42.3601";
     longitude = lib.mkDefault "-71.0589";
-    temperature.day = 5900;
+    temperature.day = 6500;
     temperature.night = 3700;
   };
 }
