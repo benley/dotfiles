4badf92f3bb3e4f06143262f07004892b420f433
Benjamin Staffin <benley@gmail.com>
2020-06-25 14:19:53 -0400

    udev-mode round 2: derived from fundamental-mode

--- cfg/.emacs.d/elisp/udev-mode.el
+++ cfg/.emacs.d/elisp/udev-mode.el
@@ -1,17 +1,94 @@
-;;; udev-mode --- Summary
+;;; udev-mode --- Major mode for editing udev rules files
+
+;; Author: Benjamin Staffin <benley@gmail.com>
+;; Keywords: udev linux
+
 ;;; Commentary:
+
 ;;; Code:
+
 ;; -*- lexical-binding: t; -*-
 
-(require 'generic-x)
+(defvar udev-match-keywords
+  '("ACTION"
+    "DEVPATH"
+    "KERNEL"
+    "NAME"
+    "SYMLINK"
+    "SUBSYSTEM"
+    "DRIVER"
+    "KERNELS"
+    "SUBSYSTEMS"
+    "DRIVERS"
+    "TAGS"
+    "TAG"
+    "TEST"
+    ;; "PROGRAM"  ;; this is special - uses PROGRAM="" even though it's a matcher, not a setter
+    "RESULT"))
 
-(define-generic-mode
-    'udev-mode
-  '("#")  ;; comments start with #
-  '("ATTRS" "ENV" "SUBSYSTEM" "TAG" "MODE" "GROUP" "KERNEL")  ;; some keywords
-  '(("\\(==|+=|!=|=\\)" 1 'font-lock-operator)
-    ("{\\([A-Za-z0-9_]+\\)}" 1 font-lock-variable-name-face)
-    )
-  '("\\.rules\\'")  ;; files for which to activate this mode
-  nil ;; other functions to call
-  "A mode for udev rules files")
+(defvar udev-matchparam-keywords
+  '("ATTR"
+    "SYSCTL"
+    "ATTRS"
+    "ENV"
+    "CONST"
+    "TEST"))
+
+(defvar udev-assign-keywords
+  '("NAME"
+    "SYMLINK"
+    "OWNER"
+    "GROUP"
+    "MODE"
+    "TAG"
+    "LABEL"
+    "GOTO"
+    "OPTIONS"))
+
+(defvar udev-assignparam-keywords
+  '("SECLABEL"
+    "ATTR"
+    "SYSCTL"
+    "ENV"
+    "RUN"  ;; {(program|builtin)}
+    "IMPORT"  ;; {(program|builtin|file|db|cmdline|parent)}
+    ))
+
+(defvar udev-font-lock-defaults
+  `(((,(concat (regexp-opt udev-match-keywords 'words)
+               (regexp-opt '("==" "!=")))
+      (1 font-lock-constant-face))
+
+     (,(concat (regexp-opt udev-matchparam-keywords 'words)
+               "{\\([_A-Za-z0-9]+\\)}"
+               (regexp-opt '("==" "!=")))
+      (1 font-lock-constant-face)
+      (2 font-lock-variable-name-face))
+
+     (,(concat (regexp-opt udev-assign-keywords 'words)
+               (regexp-opt '("+=" "-=" ":=" "=")))
+      (1 font-lock-builtin-face))
+
+     (,(concat (regexp-opt udev-assignparam-keywords 'words)
+               "{\\([_A-Za-z0-9]+\\)}"
+               (regexp-opt '("+=" "-=" ":=" "=")))
+      (1 font-lock-builtin-face)
+      (2 font-lock-variable-name-face))
+
+     ("\\(PROGRAM\\)=" . (1 font-lock-constant-face)))))
+
+;;;###autoload
+(define-derived-mode udev-mode fundamental-mode "udev"
+  (setq font-lock-defaults udev-font-lock-defaults)
+  (setq comment-start "#")
+  (setq comment-end "")
+
+  (modify-syntax-entry ?# "< b" udev-mode-syntax-table)
+  (modify-syntax-entry ?\n "> b" udev-mode-syntax-table))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist (cons "\\.rules\\'" 'udev-mode))
+
+(provide 'udev-mode)
+
+;;; udev-mode.el ends here
