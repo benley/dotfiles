19f39f8fa9506c68b7ddf73a06ee8ba3561bec27
Benjamin Staffin <benley@gmail.com>
2020-06-25 14:49:11 -0400

    udev-mode: elisp tweaks

--- cfg/.emacs.d/elisp/udev-mode.el
+++ cfg/.emacs.d/elisp/udev-mode.el
@@ -9,7 +9,7 @@
 
 ;; -*- lexical-binding: t; -*-
 
-(defvar udev-match-keywords
+(defconst udev-match-keywords
   '("ACTION"
     "DEVPATH"
     "KERNEL"
@@ -26,7 +26,7 @@
     ;; "PROGRAM"  ;; this is special - uses PROGRAM="" even though it's a matcher, not a setter
     "RESULT"))
 
-(defvar udev-matchparam-keywords
+(defconst udev-matchparam-keywords
   '("ATTR"
     "SYSCTL"
     "ATTRS"
@@ -34,7 +34,7 @@
     "CONST"
     "TEST"))
 
-(defvar udev-assign-keywords
+(defconst udev-assign-keywords
   '("NAME"
     "SYMLINK"
     "OWNER"
@@ -45,7 +45,7 @@
     "GOTO"
     "OPTIONS"))
 
-(defvar udev-assignparam-keywords
+(defconst udev-assignparam-keywords
   '("SECLABEL"
     "ATTR"
     "SYSCTL"
@@ -54,37 +54,42 @@
     "IMPORT"  ;; {(program|builtin|file|db|cmdline|parent)}
     ))
 
-(defvar udev-font-lock-defaults
-  `(((,(concat (regexp-opt udev-match-keywords 'words)
-               (regexp-opt '("==" "!=")))
-      (1 font-lock-constant-face))
+(defconst udev-font-lock-keywords
+  `((,(concat (regexp-opt udev-match-keywords 'words)
+              (regexp-opt '("==" "!=")))
+     (1 font-lock-constant-face))
 
-     (,(concat (regexp-opt udev-matchparam-keywords 'words)
-               "{\\([_A-Za-z0-9]+\\)}"
-               (regexp-opt '("==" "!=")))
-      (1 font-lock-constant-face)
-      (2 font-lock-variable-name-face))
+    (,(concat (regexp-opt udev-matchparam-keywords 'words)
+              "{\\([_A-Za-z0-9]+\\)}"
+              (regexp-opt '("==" "!=")))
+     (1 font-lock-constant-face)
+     (2 font-lock-variable-name-face))
 
-     (,(concat (regexp-opt udev-assign-keywords 'words)
-               (regexp-opt '("+=" "-=" ":=" "=")))
-      (1 font-lock-builtin-face))
+    (,(concat (regexp-opt udev-assign-keywords 'words)
+              (regexp-opt '("+=" "-=" ":=" "=")))
+     (1 font-lock-builtin-face))
 
-     (,(concat (regexp-opt udev-assignparam-keywords 'words)
-               "{\\([_A-Za-z0-9]+\\)}"
-               (regexp-opt '("+=" "-=" ":=" "=")))
-      (1 font-lock-builtin-face)
-      (2 font-lock-variable-name-face))
+    (,(concat (regexp-opt udev-assignparam-keywords 'words)
+              "{\\([_A-Za-z0-9]+\\)}"
+              (regexp-opt '("+=" "-=" ":=" "=")))
+     (1 font-lock-builtin-face)
+     (2 font-lock-variable-name-face))
 
-     ("\\(PROGRAM\\)=" . (1 font-lock-constant-face)))))
+    ("\\(PROGRAM\\)=" . (1 font-lock-constant-face))))
 
 ;;;###autoload
 (define-derived-mode udev-mode fundamental-mode "udev"
-  (setq font-lock-defaults udev-font-lock-defaults)
+  (setq font-lock-defaults '(udev-font-lock-keywords
+                             nil ;; keywords-only
+                             nil ;; case-fold
+                             nil ;; syntax-alist
+                             nil ;; other-vars
+                             ))
   (setq comment-start "#")
   (setq comment-end "")
 
-  (modify-syntax-entry ?# "< b" udev-mode-syntax-table)
-  (modify-syntax-entry ?\n "> b" udev-mode-syntax-table))
+  (modify-syntax-entry ?# "<" udev-mode-syntax-table)
+  (modify-syntax-entry ?\n ">" udev-mode-syntax-table))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist (cons "\\.rules\\'" 'udev-mode))
