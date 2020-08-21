d5a46235a9ff80f4187784b7655ec85b65f31ef8
Benjamin Staffin <benley@gmail.com>
2020-08-21 12:39:08 -0400

    avahi defaults

--- machines/imports/defaults.nix
+++ machines/imports/defaults.nix
@@ -125,8 +125,8 @@
     ipv4 = true;
     ipv6 = true;
     nssmdns = true;
-    publish.enable = true;
-    publish.addresses = true;
+    publish.enable = lib.mkDefault true;
+    publish.addresses = lib.mkDefault true;
   };
 
   services.irqbalance.enable = true;
