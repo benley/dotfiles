0678de1ca785d52722aedd19c16069db337a658a
Benjamin Staffin <benley@gmail.com>
2024-02-02 17:00:47 -0500

    enable nix flakes

--- machines/imports/nix.nix
+++ machines/imports/nix.nix
@@ -9,6 +9,7 @@
     ];
     daemonIOSchedClass = "idle";
     settings = {
+      experimental-features = [ "nix-command" "flakes" ];
       keep-outputs = true;
       keep-derivations = true;
       sandbox = true;
