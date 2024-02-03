2339bd543626fe62ebbb8093e67f019efe3d776a
Benjamin Staffin <benley@gmail.com>
2024-02-03 03:00:43 -0500

    split out a prometheus module

--- machines/nyanbox/configuration.nix
+++ machines/nyanbox/configuration.nix
@@ -20,6 +20,7 @@ with rec {
     ./modules/nyanbox-backups.nix
     ./modules/netbox.nix
     ./modules/paperless.nix
+    ./modules/prometheus.nix
     ./modules/vaultwarden.nix
   ];
 
@@ -27,6 +28,7 @@ with rec {
   my.keycloak.enable = true;
   my.netbox.enable = true;
   my.paperless.enable = true;
+  my.prometheus.enable = true;
   my.vaultwarden.enable = true;
 
   boot.loader.efi.canTouchEfiVariables = true;
@@ -89,7 +91,6 @@ with rec {
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [
-      9090 # Prometheus
       80 443
       54191 # Transmission peering
       139 445 # Samba
@@ -134,112 +135,6 @@ with rec {
   #     UPDATE_MODS_ON_START = "false";
   #   };
   # };
-
-  services.prometheus = {
-    enable = true;
-    globalConfig = {
-      scrape_interval = "15s";
-      evaluation_interval = "15s";
-      scrape_timeout = "10s";
-    };
-    scrapeConfigs = [
-      {
-        job_name = "prometheus";
-        static_configs = [{
-          targets = ["localhost:9090"];
-        }];
-      }
-      {
-        job_name = "node";
-        static_configs = [{
-          targets = [
-            "192.168.7.24:9100"  # nyanbox
-            "192.168.7.36:9100"  # ditto / homeassistant
-            "pve0.zoiks.net:9100"
-          ];
-        }];
-      }
-      {
-        job_name = "grafana";
-        static_configs = [{
-          targets = ["localhost:3000"];
-        }];
-      }
-      {
-        job_name = "hass";
-        scrape_interval = "15s";
-        metrics_path = "/api/prometheus";
-        bearer_token = secrets.hass_bearer_token;
-        static_configs = [{
-          targets = ["192.168.7.36:8123"];
-        }];
-      }
-      {
-        job_name = "blackbox-http";
-        metrics_path = "/probe";
-        params = {
-          module = ["http_2xx"];
-        };
-        static_configs = [{
-          targets = [
-            "http://google.com"
-            "https://hass.zoiks.net/"
-          ];
-        }];
-        relabel_configs = [
-          {
-            source_labels = ["__address__"];
-            target_label = "__param_target";
-          }
-          {
-            source_labels = ["__param_target"];
-            target_label = "instance";
-          }
-          {
-            target_label = "__address__";
-            replacement = "nyanbox.zoiks.net:9115";
-          }
-        ];
-      }
-      {
-        job_name = "blackbox-ping";
-        metrics_path = "/probe";
-        params = {
-          module = ["icmp"];
-        };
-        static_configs = [{
-          targets = [
-            "osric.zoiks.net"
-            "8.8.8.8"
-            "2001:4860:4860::8888"
-          ];
-        }];
-        relabel_configs = [
-          {
-            source_labels = ["__address__"];
-            target_label = "__param_target";
-          }
-          {
-            source_labels = ["__param_target"];
-            target_label = "instance";
-          }
-          {
-            target_label = "__address__";
-            replacement = "nyanbox.zoiks.net:9115";
-          }
-        ];
-      }
-    ];
-    ruleFiles = [
-      ./prometheus-node-rules.yml
-      ./hass-rules.yml
-    ];
-    extraFlags = [
-      "--storage.tsdb.retention=30d"
-      "--web.route-prefix=/"
-    ];
-    webExternalUrl = "https://nyanbox.zoiks.net/prometheus";
-  };
 
   services.prometheus.exporters.node = {
     enable = true;
@@ -297,7 +192,6 @@ with rec {
     resolver.addresses = [ "127.0.0.1" ];
     # proxyResolveWhileRunning = true;
     upstreams = {
-      prometheus.servers = { "127.0.0.1:9090" = {}; };
       transmission.servers = { "127.0.0.1:9091" = {}; };
       home-assistant.servers = { "192.168.7.36:8123" = {}; };
       nextcloud.servers = { "192.168.7.181:9001" = {}; };
@@ -355,11 +249,6 @@ with rec {
 
         locations."/" = {
           tryFiles = "$uri $uri/ =404";
-        };
-
-        locations."/prometheus/" = {
-          proxyPass = "http://prometheus/";
-          extraConfig = rootExtraConfig;
         };
 
         locations."/transmission/" = {
---
+++ machines/nyanbox/modules/prometheus.nix
@@ -0,0 +1,129 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.my.prometheus;
+  secrets = import ../secrets.nix;
+in
+{
+  options.my.prometheus.enable = lib.mkEnableOption "prometheus";
+
+  config = lib.mkIf cfg.enable {
+
+    services.nginx = {
+      upstreams.prometheus.servers = { "127.0.0.1:9090" = {}; };
+      virtualHosts."nyanbox.zoiks.net" = let rootExtraConfig = config.services.nginx.virtualHosts."nyanbox.zoiks.net".locations."/".extraConfig; in {
+        locations."/prometheus/" = {
+          proxyPass = "http://prometheus/";
+          extraConfig = rootExtraConfig;
+        };
+      };
+    };
+
+    services.prometheus = {
+      enable = true;
+      globalConfig = {
+        scrape_interval = "15s";
+        evaluation_interval = "15s";
+        scrape_timeout = "10s";
+      };
+      scrapeConfigs = [
+        {
+          job_name = "prometheus";
+          static_configs = [{
+            targets = ["localhost:9090"];
+          }];
+        }
+        {
+          job_name = "node";
+          static_configs = [{
+            targets = [
+              "192.168.7.24:9100"  # nyanbox
+              "192.168.7.36:9100"  # ditto / homeassistant
+              "pve0.zoiks.net:9100"
+            ];
+          }];
+        }
+        {
+          job_name = "grafana";
+          static_configs = [{
+            targets = ["localhost:3000"];
+          }];
+        }
+        {
+          job_name = "hass";
+          scrape_interval = "15s";
+          metrics_path = "/api/prometheus";
+          bearer_token = secrets.hass_bearer_token;
+          static_configs = [{
+            targets = ["192.168.7.36:8123"];
+          }];
+        }
+        {
+          job_name = "blackbox-http";
+          metrics_path = "/probe";
+          params = {
+            module = ["http_2xx"];
+          };
+          static_configs = [{
+            targets = [
+              "http://google.com"
+              "https://hass.zoiks.net/"
+            ];
+          }];
+          relabel_configs = [
+            {
+              source_labels = ["__address__"];
+              target_label = "__param_target";
+            }
+            {
+              source_labels = ["__param_target"];
+              target_label = "instance";
+            }
+            {
+              target_label = "__address__";
+              replacement = "nyanbox.zoiks.net:9115";
+            }
+          ];
+        }
+        {
+          job_name = "blackbox-ping";
+          metrics_path = "/probe";
+          params = {
+            module = ["icmp"];
+          };
+          static_configs = [{
+            targets = [
+              "osric.zoiks.net"
+              "8.8.8.8"
+              "2001:4860:4860::8888"
+            ];
+          }];
+          relabel_configs = [
+            {
+              source_labels = ["__address__"];
+              target_label = "__param_target";
+            }
+            {
+              source_labels = ["__param_target"];
+              target_label = "instance";
+            }
+            {
+              target_label = "__address__";
+              replacement = "nyanbox.zoiks.net:9115";
+            }
+          ];
+        }
+      ];
+      ruleFiles = [
+        ../prometheus-node-rules.yml
+        ../hass-rules.yml
+      ];
+      extraFlags = [
+        "--storage.tsdb.retention=30d"
+        "--web.route-prefix=/"
+      ];
+      webExternalUrl = "https://nyanbox.zoiks.net/prometheus";
+    };
+
+  };
+}
