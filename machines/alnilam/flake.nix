ffd8ac606ad531617633b760c9914051a2a0628d
Benjamin Staffin <benley@gmail.com>
2024-08-14 16:54:55 -0400

    Move alnilam to its own flake in a subdir

--- machines/alnilam/flake.nix
+++ machines/alnilam/flake.nix
@@ -16,19 +16,19 @@
     };
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
+
+    memsql-provisioning = {
+      url = "git+ssh://git@gitlab.cloud.memcompute.com/cloud/provisioning.git?ref=nix-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
   outputs = {self, nixpkgs, ...}@inputs: {
-    nixosConfigurations.mintaka = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.alnilam = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
-      modules = [ ./machines/mintaka/configuration.nix ];
-    };
-
-    nixosConfigurations.ein = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [ ./machines/ein/configuration.nix ];
+      modules = [ ./configuration.nix ];
     };
   };
 }
