618aec5e988e5bd09744c94eea2b9d04f906f9d7
Benjamin Staffin <benley@gmail.com>
2025-03-03 14:21:59 -0500

    cleanup some junk

--- bashrc.d/aliases.sh
+++ bashrc.d/aliases.sh
@@ -1,9 +1,7 @@
 # -*- sh-shell: bash -*-
-alias getenv='source "$HOME"/.ssh/.getenv'
 alias grep='grep --color'
 alias wat='type -a'
 alias uptig='tig ..@{u}'
-alias ip='ip -color=auto'
 
 ssh-keyrm() {
   if [[ $# -eq 1 ]]; then
